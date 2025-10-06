283e2737461fab509f4b5b9bab84b7ab28926977
Mihnea <mihnea@myownsummer.co.uk>
2025-10-06 10:04:47 +0100

    Update age validation.py

--- age validation.py
+++ age validation.py
@@ -1,9 +1,10 @@
 age = int(input("What is your age? " ))
-while age > 0  and age > 130:
+while age < 0 or age > 130:
     print("Your age is wrong")
     age = int(input("What is your age? " ))
     break
 print("You entered correct age")
+
 
 
 
