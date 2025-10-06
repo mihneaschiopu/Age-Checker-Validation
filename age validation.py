ebe696afdc4cdbb71e347ed39aa4f135ce7f1a2e
Mihnea <mihnea@myownsummer.co.uk>
2025-10-06 09:24:27 +0100

    Add files via upload

---
+++ age validation.py
@@ -0,0 +1,9 @@
+age = int(input("What is your age? " ))
+while age > 0  and age > 130:
+    print("Your age is wrong")
+    age = int(input("What is your age? " ))
+    break
+print("You entered correct age")
+
+
+
